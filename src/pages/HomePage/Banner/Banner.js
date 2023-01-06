1e64a0a4d3a4931bed0759a112e923b89e873f9c
Arafat Hossaine <arafathossain7960@gmail.comm>
2023-01-06 07:05:48 +0600

    created banner and categrory section

--- src/pages/HomePage/Banner/Banner.js
+++ src/pages/HomePage/Banner/Banner.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import image1 from '../../../media/images/bed.png';
+import image2 from '../../../media/images/chair.png';
+
+import image4 from '../../../media/images/sofa.png';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>this is a banner</h2>
+        <div className='grid lg:grid-cols-2 text-accent  '>
+           
+           <div className=''>
+                <h2>Hello this is a title</h2>
+                <h2>Hello this is a title</h2>
+                <h2>Hello this is a title</h2>
+           </div>
+           <div className=''>
+                <h1>this is a another page</h1>
+                <img src={image1} alt="" />
+
+           </div>
         </div>
     );
 };
---
+++ src/pages/HomePage/Category/Category.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Category.css';
+const Category = () => {
+
+    return (
+        <div id='category'>
+            <h1 className='my-6 text-center bold' >Category</h1>
+            
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Category;
